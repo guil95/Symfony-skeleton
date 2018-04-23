7cb1211685379a47aab7faaabdb678ee68773fd7
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-04-23 09:58:09 -0300

    Adicionando novos metodos

--- src/Controller/TesteController.php
+++ src/Controller/TesteController.php
@@ -2,10 +2,12 @@
 namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class TesteController
 {
+    //Get simples
      /**
       * @Route("/teste")
      */
@@ -14,5 +16,48 @@ class TesteController
         return new JsonResponse([
             "message" => "Teste"
         ]);
+    }
+
+    //Post simples
+    /**
+     * @Route("/testepost",methods={"POST"})
+     */
+    public function teste2(Request $request)
+    {
+        $content = $request->getContent();
+        return new JsonResponse([
+            "message" => $content
+        ]);
+    }
+
+    //GET PASSANDO PARAMETROS POSSIVEIS
+    /**
+     * @Route("/{_locale}", defaults={"_locale"="en"}, requirements={
+     *     "_locale"="en|fr|br"
+     * }, methods="GET")
+     */
+    public function homepage($_locale)
+    {
+        return new JsonResponse([
+            "message" => $_locale
+        ]);
+    }
+
+    //PUT simples
+    /**
+     * @Route("/api/posts/{id}", methods="PUT")
+     */
+    public function edit($id)
+    {
+        // ... edit a post
+    }
+
+    //DELETE simples
+    /**
+     * @Route("/api/posts/{id}", methods="DELETE")
+     */
+    public function delete($id)
+    {
+        // ... edit a post
     }
 }
